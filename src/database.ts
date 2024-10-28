e167c186c3386746b45a795d9bf77fd863a20fad
vitorperili <vitor_eduardo1997@hotmail.com>
2024-10-28 13:07:13 -0300

    chore: Production config

--- src/database.ts
+++ src/database.ts
@@ -5,10 +5,13 @@ if (!process.env.DATABASE_URL) {
   throw new Error('DATABASE_URL env not.')
 }
 export const config: Knex.Config = {
-  client: 'sqlite',
-  connection: {
-    filename: env.DATABASE_URL,
-  },
+  client: env.DATABASE_CLIENT,
+  connection: 
+    env.DATABASE_CLIENT === 'sqlite' 
+    ?{
+      filename: env.DATABASE_URL,
+    }
+    : env.DATABASE_URL,
   useNullAsDefault: true,
   migrations: {
     extension: 'ts',
